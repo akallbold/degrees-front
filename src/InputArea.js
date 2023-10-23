0fc0bfbea86d100c2f09425c699a2261755c755e
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2023-10-22 17:29:41 -0700

    prettier fixes

--- src/InputArea.js
+++ src/InputArea.js
@@ -27,7 +27,6 @@ function InputArea() {
     return () => {
       window.removeEventListener("keydown", handleKeyPress);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
