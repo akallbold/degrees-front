9f37b92c546af83bdb9ba22729703054b0b391ac
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2023-11-07 19:52:00 -0800

    styling

--- src/InputArea.js
+++ src/InputArea.js
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import { Button } from "@mui/material";
-import useDegrees from "./hooks/useDegrees";
+import { useEffect, useState } from 'react';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+import useDegrees from './hooks/useDegrees';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 
 function InputArea() {
-  const [input1, setInput1] = useState("");
-  const [input2, setInput2] = useState("");
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
   const { fetchData } = useDegrees();
 
   const handleSubmit = () => {
@@ -17,45 +17,48 @@ function InputArea() {
 
   useEffect(() => {
     const handleKeyPress = (event) => {
-      if (event.key === "Enter") {
+      if (event.key === 'Enter') {
         handleSubmit();
       }
     };
 
-    window.addEventListener("keydown", handleKeyPress);
+    window.addEventListener('keydown', handleKeyPress);
 
     return () => {
-      window.removeEventListener("keydown", handleKeyPress);
+      window.removeEventListener('keydown', handleKeyPress);
     };
   }, []);
 
   return (
-    <Box
-      component="form"
-      sx={{
-        "& > :not(style)": { m: 1, width: "25ch" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField
-        id="standard-basic"
-        label="Starting Name"
-        variant="standard"
-        value={input1}
-        onChange={(e) => setInput1(e.target.value)}
-        placeholder="Kevin Bacon"
-      />
-      <TextField
-        id="standard-basic"
-        label="Ending Name"
-        variant="standard"
-        value={input2}
-        onChange={(e) => setInput2(e.target.value)}
-        placeholder="Demi Moore"
-      />
-      <Button onClick={handleSubmit}>Run</Button>
-    </Box>
+    <Grid2 flexDirection="column" display="flex">
+      <Grid2 flexDirection="row" display="flex" sx={{ width: '100%' }} alignItems="center" justifyContent="center">
+        <Grid2 item p={3} sx={{ width: '40%' }}>
+          <TextField
+            id="standard-basic"
+            label="Starting Name"
+            variant="standard"
+            value={input1}
+            onChange={(e) => setInput1(e.target.value)}
+            placeholder="Kevin Bacon"
+          />
+        </Grid2>
+        <Grid2 item p={3} sx={{ width: '40%' }}>
+          <TextField
+            id="standard-basic"
+            label="Ending Name"
+            variant="standard"
+            value={input2}
+            onChange={(e) => setInput2(e.target.value)}
+            placeholder="Demi Moore"
+          />
+        </Grid2>
+      </Grid2>
+      <Grid2 item p={3} sx={{ width: '100%' }}>
+        <Button variant="contained" onClick={handleSubmit} sx={{ width: '100%' }}>
+          Run
+        </Button>
+      </Grid2>
+    </Grid2>
   );
 }
 
